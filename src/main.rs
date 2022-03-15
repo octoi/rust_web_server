82a608c4120bde24b62e1927e7d582d6c2c724c3
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-15 07:39:16 +0530

    accepting connection using TcpStream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer);
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
